5c7550888d6e03f09658ea0d308de6ca4a1498f0
dasotelo <sotelo.d.a@gmail.com>
2017-04-17 07:47:50 -0700

    Add files via upload

---
+++ Wk_2_Proj_DA.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 08:27:34 2017
+
+@author: sotelo
+"""
+
+import pandas
+import numpy
+import scipy.stats
+
+#Read NESARC data file into DataFrame
+proj=pandas.read_csv('nesarc_pds.csv',low_memory=False)
+
+#Convert variables of interest to numeric
+proj['MARITAL']=proj['MARITAL'].convert_objects(convert_numeric=True)
+proj['S4AQ8BR']=proj['S4AQ8BR'].convert_objects(convert_numeric=True)
+proj['S4AQ8BR']=proj['S4AQ8BR'].fillna(0)
+
+#Map marital status values to discernable string categories
+#Eliminate unknown values of major depression in last 12 mos variable
+map1={
+    1:'Married',
+    2:'Co-habitate',
+    3:'Widowed',
+    4:'Divorced',
+    5:'Separated',
+    6:'Never Married'}
+proj['MARITL_STS']=proj['MARITAL'].map(map1)
+map2={
+    0:'No',    
+    1:'Yes',
+    2:'No',
+    9:'NA'}
+proj['DEP_STS_12MO']=proj['S4AQ8BR'].map(map2)
+
+#Subset data to variables of interest only, eliminate unknown and na values
+proj_ed=proj.loc[(proj['DEP_STS_12MO']!='NA'),['MARITL_STS','DEP_STS_12MO']]
+
+#Crosstab of counts
+ct1=pandas.crosstab(proj_ed['DEP_STS_12MO'],proj_ed['MARITL_STS'])
+print('Crosstab: Counts')
+print(ct1)
+
+#Crosstab of percentages
+colsum=ct1.sum(axis=0)
+pct1=ct1/colsum
+print('Crosstab: Percentages by Column')
+print(pct1)
+
+#Chi-Square statistic
+proj_chisq=scipy.stats.chi2_contingency(ct1)
+print('Chi Squared Statistic Summary for Depression Status on Marital Status')
+print(proj_chisq)
+
+for i in range(0,6):
+    for j in range (0,6):
+        if i<j:           
+            #Crosstab of counts, subsetting data
+            rct1=ct1.iloc[:,[i,j]]
+            print('@@@@@@@@@@@@@@@@@@@@@@')
+            print(list(rct1.columns.values))
+            print('@@@@@@@@@@@@@@@@@@@@@@')
+            print('Crosstab: Counts')
+            print(rct1)
+            
+            #Crosstab of percentages
+            rcolsum=rct1.sum(axis=0)
+            rpct1=rct1/rcolsum
+            print('Crosstab: Percentages by Column')
+            print(rpct1)
+            
+            #Chi-Square statistic
+            rproj_chisq=scipy.stats.chi2_contingency(rct1)
+            print('Chi Squared Statistic Summary for Depression Status on',
+                  ' Marital Status')
+            print(rproj_chisq)
+        
